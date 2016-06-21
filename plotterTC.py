6b6259af1a1e233cf25011ae37f9140e8e9a9be3
Oleksiy1990 <oleksiy1990@gmail.com>
2016-06-21 18:11:33 +0200

    Plotter set up using Pandas
    I changed the plotter code so that it selects and analyzes data using
    Pandas now. This seems to work well

--- plotterTC.py
+++ plotterTC.py
@@ -1,4 +1,5 @@
 import tables
+import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np 
 import sys
@@ -13,34 +14,73 @@ datafile = tables.open_file(filename,"r")
 a_widths_mm = [2,5,8,11,14,17]
 b_widths_mm = [1,2,3,4,5]
 
+ab_combinations = list(itertools.product(a_widths_mm,b_widths_mm))
+
 node = "/a14b5/A"
-#data_group="/grad1.150/10_3/"
+nodes = ["/a%.ib%.i/A"%(u[0],u[1]) for u in ab_combinations]
 
-# xp_init = []
-# xp_final = []
-# yp_init = []
-# yp_final = []
-# zp_init = []
-# zp_final = []
+#def load_node(node):
+#    x_in = datafile.get_node(node).read(field="init_x")
+#    y_in = datafile.get_node(node).read(field="init_y")
+#    vx_in = datafile.get_node(node).read(field="init_vx")
+#    vy_in = datafile.get_node(node).read(field="init_vy")
+#    speed_fin = datafile.get_node(node).read(field="final_speed_xy")
+#    return [x_in,y_in,vx_in,vy_in,speed_fin]
 
-# xv_init = []
-# xv_final = []
-# yv_init = [] 
-# yv_final = []
-# zv_init = []
-# zv_final = []
+
+
+loaded_nodes = [pd.read_hdf(filename,key=node,mode="r") for node in nodes] # we have to give it a file 
+#name, the ndoe to read, and tell it to do it read-only
+
+
+
+"""
+We load the HDF5 which has beed read before as a DataFrame (using Pandas)
+
+We then check condition by condition to narrow down and select the data that we like 
+"""
+for index,u in enumerate(loaded_nodes):
+
+    df1 = pd.DataFrame(u,columns=list(("init_x","init_y","init_vx","init_vy","final_speed_xy"))) 
+    df1 = df1[df1['init_x'] < 1e-3]
+    df1 = df1[df1['init_y'] < 1e-3]
+    df1 = df1[df1['init_vy'] < 1e-3]
+    #df1 = df1[df1['init_vx'] > 2]
+    #df1 = df1[df1['init_vx'] < 10]
+    df1 = df1[df1['final_speed_xy'] < 1.5]
+    #print(df1)
+    
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    ax.scatter(np.sqrt(df1["init_vx"]**2 + df1["init_vx"]**2),df1["final_speed_xy"])
+    ax.set_title("a%.i b%.i"%ab_combinations[index])
+    plt.show()
+sys.exit(0)
+
+
+"""
+
+
+Notes: 
+"index" apparently means rows and "columns" are as it sounds in Pandas
+
+"""
+
+df1 = df1[df1['final_speed_xy'] < 2]
+q = df1[df1['final_speed_xy'] > 1]
+#q = df1.loc[lambda df1: df1.final_speed_xy < 9, :]
+print(q)
+
+sys.exit(0)
+
 
 """
 
 Loading data from any given node
 """
-def load_node(node):
-    x_in = datafile.get_node(node).read(field="init_x")
-    y_in = datafile.get_node(node).read(field="init_y")
-    vx_in = datafile.get_node(node).read(field="init_vx")
-    vy_in = datafile.get_node(node).read(field="init_vy")
-    speed_fin = datafile.get_node(node).read(field="final_speed_xy")
-    return [x_in,y_in,vx_in,vy_in,speed_fin]
+
 
 nodes = ["/a%.ib%.i/A"%(u[0],u[1]) for u in itertools.product(a_widths_mm,b_widths_mm)]
 print(nodes)
